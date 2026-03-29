509ff42ab9bf8281a17c288f320250511f65962a
Anjali Surya <surya.15@buckeyemail.osu.edu>
2026-03-29 17:30:04 -0400

    Configure EF Core

--- Operation-Rowdy-Cougar.Api/Program.cs
+++ Operation-Rowdy-Cougar.Api/Program.cs
@@ -1,8 +1,21 @@
+using Operation.Rowdy.Cougar.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
+
+// Add Entity Framework services
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite("Data Source=../Registrar.sqlite",
+    b => b.MigrationsAssembly("Operation.Rowdy.Cougar.Api")));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
