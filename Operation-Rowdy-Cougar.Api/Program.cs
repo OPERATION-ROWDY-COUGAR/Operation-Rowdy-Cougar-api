ad3de1dd7aa54594501289a60cefaf48297aa625
Sarah Feairheller <feairheller.3@buckeyemail.osu.edu>
2026-04-19 13:42:09 -0400

    Add CORS support - ended on end of page 6

--- Operation-Rowdy-Cougar.Api/Program.cs
+++ Operation-Rowdy-Cougar.Api/Program.cs
@@ -6,7 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source=../Registrar.sqlite"));
+    options.UseSqlite("Data Source=../Registrar.sqlite",
+    b => b.MigrationsAssembly("Operation-Rowdy-Cougary.Api"))
+);
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder =>{
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,5 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
+
+app.UseCors();
 
 app.Run();
